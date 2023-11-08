f86307cf8a7be349124f9dfc90a749d36d6fe5da
Brendan Huddleston <brendanhuddleston18@gmail.com>
2023-11-07 18:34:15 -0600

    added table to show when results event is clicked, looped over products and pushed into allPics array

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
 let picContainer = document.getElementById('pictures');
-let reportContainer = document.getElementById('report');
+let reportContainer = document.getElementById('chart');
 let button = document.getElementById('results');
 
 let image1 = document.querySelector('#pictures img:first-child');
@@ -12,7 +12,7 @@ let state = {
   currentClicks: 0,
   totalClicks: 25,
   allPics: [],
-}
+};
 // console.log(state.allPics);
 // console.log(state.allPics.votes)
 // debugger;
@@ -132,25 +132,31 @@ function listeners(){
   button.addEventListener("click", showResults);
 }
 
-new Product('R2D2 Bag', 'img/bag.jpg');
-new Product('Banana', 'img/banana.jpg');
-new Product('Bathroom', 'img/bathroom.jpg');
-new Product('Boots', 'img/boots.jpg');
-new Product('Breakfast', 'img/breakfast.jpg');
-new Product('Bubblegum', 'img/bubblegum.jpg');
-new Product('Chair', 'img/chair.jpg');
-new Product('Cthulhu', 'img/cthulhu.jpg');
-new Product('Dog Duck', 'img/dog-duck.jpg');
-new Product('Dragon', 'img/dragon.jpg');
-new Product('Pen', 'img/pen.jpg');
-new Product('Pet Sweep', 'img/pet-sweep.jpg');
-new Product('Scissors', 'img/scissors.jpg');
-new Product('Shark', 'img/shark.jpg');
-new Product('Sweep', 'img/sweep.png');
-new Product('Tauntaun', 'img/tauntaun.jpg');
-new Product('Unicorn', 'img/unicorn.jpg');
-new Product('Water Can', 'img/water-can.jpg');
-new Product('Wine Glass', 'img/wine-glass.jpg');
+let productList = [
+  new Product('R2D2 Bag', 'img/bag.jpg'),
+  new Product('Banana', 'img/banana.jpg'),
+  new Product('Bathroom', 'img/bathroom.jpg'),
+  new Product('Boots', 'img/boots.jpg'),
+  new Product('Breakfast', 'img/breakfast.jpg'),
+  new Product('Bubblegum', 'img/bubblegum.jpg'),
+  new Product('Chair', 'img/chair.jpg'),
+  new Product('Cthulhu', 'img/cthulhu.jpg'),
+  new Product('Dog Duck', 'img/dog-duck.jpg'),
+  new Product('Dragon', 'img/dragon.jpg'),
+  new Product('Pen', 'img/pen.jpg'),
+  new Product('Pet Sweep', 'img/pet-sweep.jpg'),
+  new Product('Scissors', 'img/scissors.jpg'),
+  new Product('Shark', 'img/shark.jpg'),
+  new Product('Sweep', 'img/sweep.png'),
+  new Product('Tauntaun', 'img/tauntaun.jpg'),
+  new Product('Unicorn', 'img/unicorn.jpg'),
+  new Product('Water Can', 'img/water-can.jpg'),
+  new Product('Wine Glass', 'img/wine-glass.jpg'),
+];
+
+for(let = 0; i < productList.length; i++){
+  state.allPics.push(productList[i]);
+}
 renderImages();
 listeners();
 removeButton();
